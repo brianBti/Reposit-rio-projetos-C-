8315089a063d1c5e5aed35dcd9c63476dd7c5d23
brianBti <150454096+brianBti@users.noreply.github.com>
2023-12-09 13:13:56 -0300

    Add files via upload

---
+++ Conversão temperatura/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args, Console console)
+    {
+        Console.WriteLine("Quantos graus em °C deseja converter?");
+        var celsius = Convert.ToInt32(Console.ReadLine());
+
+        var fahrenheit = celsius * 9 / 5 + 32;
+        var kelvin = celsius + 273.15;
+        console.WriteLine(celsius + " graus celsius são " + fahrenheit + " °F e " + kelvin + " Kelvin");
+    }
+}
